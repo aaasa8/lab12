b27132967211166bb90ba05c17ddafa33a0b0a5c
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-05-25 15:34:19 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/lab12/MainActivity.kt
+++ app/src/main/java/com/example/lab12/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+	latinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+			binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+			binding.button.setOnClickListener{
+				
+			}
     }
 }
